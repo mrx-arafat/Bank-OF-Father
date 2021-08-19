d696c39ec6edf2b3a6abc79ff13d59698adb6dd5
Easin Arafat <arafatmrx@gmail.com>
2021-08-19 12:20:28 +0600

    init

---
+++ The Bank of Father/js/login.js
@@ -0,0 +1,51 @@
+document.getElementById("login-submit").addEventListener("click", function () {
+  // get user email
+  const emailField = document.getElementById("user-email");
+  const userEmail = emailField.value;
+  // get user password
+  const passwordField = document.getElementById("user-password");
+  const userPassword = passwordField.value;
+  // check email and password
+  if (userEmail == "sontan@baap.com" && userPassword == "secret") {
+    window.location.href = "banking.html";
+  }
+});
+
+// modal
+
+var openmodal = document.querySelectorAll(".modal-open");
+for (var i = 0; i < openmodal.length; i++) {
+  openmodal[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    toggleModal();
+  });
+}
+
+const overlay = document.querySelector(".modal-overlay");
+overlay.addEventListener("click", toggleModal);
+
+var closemodal = document.querySelectorAll(".modal-close");
+for (var i = 0; i < closemodal.length; i++) {
+  closemodal[i].addEventListener("click", toggleModal);
+}
+
+document.onkeydown = function (evt) {
+  evt = evt || window.event;
+  var isEscape = false;
+  if ("key" in evt) {
+    isEscape = evt.key === "Escape" || evt.key === "Esc";
+  } else {
+    isEscape = evt.keyCode === 27;
+  }
+  if (isEscape && document.body.classList.contains("modal-active")) {
+    toggleModal();
+  }
+};
+
+function toggleModal() {
+  const body = document.querySelector("body");
+  const modal = document.querySelector(".modal");
+  modal.classList.toggle("opacity-0");
+  modal.classList.toggle("pointer-events-none");
+  body.classList.toggle("modal-active");
+}
